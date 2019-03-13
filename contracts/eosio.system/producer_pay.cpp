ab3550bea14ab1c884843e19228f9ead056896cf
luchiagogogo <32331016+big4david@users.noreply.github.com>
2019-03-13 18:55:29 +0800

    Update producer_pay.cpp

--- contracts/eosio.system/producer_pay.cpp
+++ contracts/eosio.system/producer_pay.cpp
@@ -13,7 +13,7 @@ namespace eosiosystem {
    const uint64_t useconds_per_day      = 24 * 3600 * uint64_t(1000000);
    const uint64_t useconds_per_year     = seconds_per_year*1000000ll;
 
-   const int64_t block_initial_timestamp = 1551369600ll;  // epoch is year 2019.03.01    1551369600s
+   const int64_t block_initial_timestamp = 1551369600ll;  // epoch year 2019.03.01    unix timestamp 1551369600s
    //yta seo total= yta_seo_year[i] * YTA_SEO_BASE
    const uint32_t YTA_SEO_BASE = 10'0000;
    const std::vector<int> yta_seo_year = {
