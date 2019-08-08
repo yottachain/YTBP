27871ab820e2b794356b7c0ef466ed05c508b842
Wang Zhi <wangxxzhiyy@126.com>
2019-08-08 18:03:53 +0800

    update hddlock contract

--- contracts/hddlock/hddlock.cpp
+++ contracts/hddlock/hddlock.cpp
@@ -11,16 +11,58 @@
 
 using namespace eosio;
 
+static constexpr eosio::name active_permission{N(active)};
+static constexpr eosio::name token_account{N(eosio.token)};
+
+
 void hddlock::init() {
 
 }
 
-void hddlock::addrule(uint64_t id, std::vector<uint64_t>& times, std::vector<uint8_t>& percentage, std::string& desc) {
+void hddlock::addrule(uint64_t lockruleid, std::vector<uint64_t>& times, std::vector<uint8_t>& percentage, std::string& desc) 
+{
 
     eosio_assert(times.size() >= 2, "invalidate size of times array");
     eosio_assert(times.size() == percentage.size(), "times and percentage in different size.");
 
+    lockrule_table _lockrule(_self, _self);
+    auto itrule = _lockrule.find(lockruleid);
+    eosio_assert(itrule == _lockrule.end(), "the id already existed in rule table");  
+
+    _lockrule.emplace(_self, [&](auto &row) {
+        row.lockruleid   = lockruleid;
+        row.times        = times;
+        row.percentage   = percentage;
+        row.desc         = desc;
+    });          
+}
+
+void hddlock::locktransfer(uint64_t lockruleid, account_name from, account_name to, asset quantity, std::string memo) 
+{
+    require_auth(from);
+    eosio_assert( quantity.symbol == CORE_SYMBOL , "only core symbole support this lock transsfer");  
+
+    lockrule_table _lockrule(_self, _self);
+    auto itrule = _lockrule.find(lockruleid);
+    eosio_assert(itrule != _lockrule.end(), "lockruleid not existed in rule table");  
+
+    action(
+       permission_level{from, active_permission},
+       token_account, N(transfer),
+       std::make_tuple(from, to, quantity, memo))
+       .send();
+
+    acclock_table _acclock(_self, to);
+    _acclock.emplace(_self, [&](auto &row) {
+        row.quantity    = quantity;
+        row.lockruleid  = lockruleid;
+        row.user        = to;
+        row.from        = from;
+        row.memo        = memo;
+        row.time        = current_time();
+    });
 }
 
 
-EOSIO_ABI( hddlock, (init)(addrule))
+
+EOSIO_ABI( hddlock, (init)(addrule)(locktransfer))
--- contracts/hddlock/hddlock.hpp
+++ contracts/hddlock/hddlock.hpp
@@ -3,6 +3,8 @@
 #include <eosiolib/singleton.hpp>
 #include <eosiolib/symbol.hpp>
 
+#include <string>
+
 
 using eosio::name;
 using eosio::asset;
@@ -17,17 +19,59 @@ class hddlock : public eosio::contract {
         using contract::contract;
 
         void init();
-        void addrule(uint64_t id, std::vector<uint64_t>& times, std::vector<uint8_t>& percentage, std::string& desc);
+        void addrule(uint64_t lockruleid, std::vector<uint64_t>& times, std::vector<uint8_t>& percentage, std::string& desc);
+        void locktransfer(uint64_t lockruleid, account_name from, account_name to, asset quantity, std::string memo);
 
+        inline asset get_lock_asset( account_name user )const;
 
     private:
 
         struct lockrule {
-            uint64_t    ruleid;
-            uint64_t    primary_key()const { return ruleid; }
+            uint64_t                lockruleid;
+            std::vector<uint64_t>   times;
+            std::vector<uint8_t>    percentage;
+            std::string             desc;
+            uint64_t    primary_key()const { return lockruleid; }
         };
         typedef multi_index<N(lockrule), lockrule> lockrule_table; 
+
+        struct acclock {
+            asset           quantity;
+            uint64_t        lockruleid;        
+            account_name    user;
+            account_name    from;
+            std::string     memo;
+            uint64_t        time;
+            uint64_t    primary_key()const { return time; }
+        };
+        typedef multi_index<N(acclock), acclock> acclock_table; 
 
 };
 
 
+asset hddlock::get_lock_asset( account_name user ) const
+{
+    acclock_table _acclock(_self, user);
+    asset lockasset{0, CORE_SYMBOL};
+
+    uint64_t curtime = current_time()/1000000ll; //seconds
+    for(auto it = _acclock.begin(); it != _acclock.end(); it++) {
+        int64_t amount = it->quantity.amount;
+        uint8_t percent = 0;
+        size_t n = 0;
+        lockrule_table _lockrule(_self, _self);
+        auto itrule = _lockrule.find(it->lockruleid);
+        eosio_assert(itrule != _lockrule.end(), "lockruleid not existed in rule table");          
+        for(auto itt = itrule->times.begin(); itt != itrule->times.end(); itt++) {
+            if(*itt > curtime ) {
+                break;
+            }
+            percent = itrule->percentage[n];
+            n++; 
+        }
+        eosio_assert(percent>=0 && percent<=100, "invalidate lock percentage");          
+        percent = 100 - percent;
+        lockasset.amount += (amount * percent)/100;
+    }
+    return lockasset;
+}
