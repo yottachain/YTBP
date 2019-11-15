9000d86e75e59bc84a00fe8d9489aa1d9510414d
Wang Zhi <wangxxzhiyy@126.com>
2019-11-15 13:19:00 +0800

    frozen user still need pay forfeit

--- contracts/eosio.token/eosio.token.cpp
+++ contracts/eosio.token/eosio.token.cpp
@@ -117,18 +117,18 @@ void token::sub_balance_yta( account_name owner, asset value , account_name to) 
    const auto& from = from_acnts.get( value.symbol.name(), "no balance object found" );
 
    bool is_frozen = hddlock(hdd_lock_account).is_frozen(owner);  
-   eosio_assert( !is_frozen, "user is frozen" );
-
 
    //todo : need consider lock_token situation
    if( to == hdd_deposit_account) { //缴纳罚金,锁仓币也可以缴纳罚金
       eosio_assert( from.balance.amount >= value.amount, "overdrawn balance" );
    } else if( to == N(eosio.stake) ) { //用来抵押带宽和CPU
+      eosio_assert( !is_frozen, "user is frozen" );
       //forfeit can not use to delegatebw and vote
       auto deposit_and_forfeit = hdddeposit(hdd_deposit_account).get_deposit(owner);
       eosio_assert( deposit_and_forfeit.symbol == value.symbol, "symbol precision mismatch" );
       eosio_assert( from.balance.amount - deposit_and_forfeit.amount >= value.amount, "overdrawn balance" );
    } else { //普通转账，需要考虑锁仓和存储抵押
+      eosio_assert( !is_frozen, "user is frozen" );
       auto frozen_asset = hdddeposit(hdd_deposit_account).get_deposit(owner);
       //also need check lock token issue
       auto lock_asset = hddlock(hdd_lock_account).get_lock_asset(owner);
